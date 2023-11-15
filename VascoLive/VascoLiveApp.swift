56a10f5b5f2ba9e557cb6abe30e8a29bd9fc38a2
Paulo Ricardo <pauloa.vieira@fornecedores.sicoob.com.br>
2023-11-15 12:59:37 -0300

    Initial Commit

---
+++ VascoLive/VascoLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  VascoLiveApp.swift
+//  VascoLive
+//
+//  Created by Paulo Ricardo de Araujo Vieira on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VascoLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
